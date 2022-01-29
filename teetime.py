f8618ebf39f946ce211efd349fb6e990affc32de
Moo Park <amp0303@gmail.com>
2022-01-29 11:18:54 -0800

    Refresh page if the time slots haven't been loaded yet

--- teetime.py
+++ teetime.py
@@ -1,5 +1,6 @@
 import Account #create separate file for username/password for golf course
 import Utility
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
@@ -9,6 +10,7 @@ import time
 driver=webdriver.Chrome(executable_path='chromedriver')
 xPathInit='//*[@id="searchMatrix"]/div[2]/table/tbody/tr['
 xPathEnd=']/td[3]'
+sysRunning = 1
 
 #Open the browser
 driver.get('https://sanjosemuni.quick18.com/teetimes/searchmatrix')
@@ -18,35 +20,42 @@ dateSelect.click()
 dateSelect.clear()
 dateSelect.send_keys(Utility.DATE) #Automated 7 days ahead from the date the script runs
 dateSelect.submit()
-time.sleep(2)
+#time.sleep(1) #figure out load time
 
+while(sysRunning == 1):
+     table=driver.find_element(By.XPATH, '//*[@id="searchMatrix"]/div[2]/table/tbody')
 
-table=driver.find_element(By.XPATH, '//*[@id="searchMatrix"]/div[2]/table/tbody')
-index = 1
-earlyTime = datetime.strptime(Utility.earlyTime, "%I:%M%p")
-latestTime = datetime.strptime(Utility.latestTime, "%I:%M%p")
-
-for row in table.find_elements(By.XPATH, './tr'): #ELEMENTS vs ELEMENT
-     teeTime=row.find_element(By.CLASS_NAME, 'mtrxTeeTimes')
-     players=row.find_element(By.CLASS_NAME, 'matrixPlayers')
-     if(Utility.checkTime(teeTime.text, players.text, earlyTime, latestTime)):
-          #print(index)
-          break
+     index = 1
+     earlyTime = datetime.strptime(Utility.earlyTime, "%I:%M%p")
+     latestTime = datetime.strptime(Utility.latestTime, "%I:%M%p")
+     rows = table.find_elements(By.XPATH, './tr') #ELEMENTS vs ELEMENT
+     if len(rows) > 0:
+          for row in rows:
+               teeTime=row.find_element(By.CLASS_NAME, 'mtrxTeeTimes')
+               players=row.find_element(By.CLASS_NAME, 'matrixPlayers')
+               if(Utility.checkTime(teeTime.text, players.text, earlyTime, latestTime)):
+                    sysRunning = 0
+                    break
+               else:
+                    index += 1
+          if len(rows) <= index:
+               print("No Times Found")
      else:
-          index += 1
+          driver.refresh()
+          time.sleep(.5)
 
 finalXpath=xPathInit + str(index) + xPathEnd #creating XPath to the confirm button on the correct time.
 picks=driver.find_element(By.XPATH, finalXpath)
 
 button=picks.find_element(By.CLASS_NAME, 'ok')
 button.click()
-time.sleep(1)
+#time.sleep(1)
 
 numPlayers=driver.find_element(By.ID, 'Players2')
 numPlayers.click()
 button=driver.find_element(By.CLASS_NAME, 'ok')
 button.click()
-time.sleep(1)
+time.sleep(.5)
 
 
 emailField=driver.find_element(By.ID, 'EmailAddress')
